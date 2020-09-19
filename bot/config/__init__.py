45c113a2c1d7609b775e1d7f43ff2a17b5cee241
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2020-09-19 16:59:39 +0300

    Added suggestionsTemplates and expanded missedTemplates

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -18,9 +18,20 @@ questionTemplate = [
     'I think it`s better to ask Google'
 ]
 
+suggestionsTemplates = [
+    'Have you tried BBQ pizza?',
+    'Have you tried Mimosa pizza?',
+    'Have you tried California pizza?',
+
+]
+
 missTemplates = [
     'I don\'t understand you. Try to talk about pizza...',
-    'What\'s the matter with it'
+    'Do you know that americans eat approximately 350 slices per second? Crazy, isn`t it?',
+    'Fun fact: 36% of all pizza orders want their pizza topped with pepperoni.',
+    'Btw, over 5 billion pizzas are sold worldwide each year.',
+    'Do you know that October has been celebrated as National Pizza month since 1987?',
+    'Fun fact: Women are more likely to order vegetarian options as opposed to men.'
 ]
 
 totalDict = {
@@ -56,6 +67,14 @@ totalDict = {
     'how': questionTemplate,
     '?': questionTemplate,
 
-    'want': ['Have you tried BBQ pizza'],
+    'recommend': suggestionsTemplates,
+    'recommended': suggestionsTemplates,
+    'suggest': suggestionsTemplates,
+    'suggested': suggestionsTemplates,
+    'want': suggestionsTemplates,
+    'wanted': suggestionsTemplates,
+    'advise': suggestionsTemplates,
+    'advised': suggestionsTemplates,
+
     'pizza': ['I love pizza']
 }
